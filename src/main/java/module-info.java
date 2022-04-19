c5e7263def4c2cdd58cd28936809d569ea447085
Mehmet Ubeyde Kara <ubeydekara@yahoo.com>
2022-04-19 03:34:19 +0300

    spring boot integration

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.nesne.acente {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires MaterialFX;
-
-    opens com.nesne.acente to javafx.fxml;
-    exports com.nesne.acente;
-}
